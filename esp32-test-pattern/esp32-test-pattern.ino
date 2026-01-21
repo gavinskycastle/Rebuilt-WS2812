ac90b5f9794923343494452fda1329ccd73ba623
Gavin P <gavinjpeeler@gmail.com>
2026-01-20 20:06:11 -0800

    Frame switching working
    need to work back up to a full 16x16

--- esp32-test-pattern/esp32-test-pattern.ino
+++ esp32-test-pattern/esp32-test-pattern.ino
@@ -2,7 +2,7 @@
 #include "test_pattern.h"
 
 #define PIN_WS2812B  21  // GPIO ESP32-S3 pin that connects to WS2812B
-#define NUM_PIXELS  256  // The number of LEDs (pixels) on WS2812B
+#define NUM_PIXELS  5  // The number of LEDs (pixels) on WS2812B
 
 Adafruit_NeoPixel WS2812B(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 
@@ -14,16 +14,18 @@ void setup() {
 }
 
 void loop() {
-
-  for(uint32_t* (*frame) : frames) {
+  
+  for(uint32_t* frame : frames) {
     WS2812B.clear();
 
-    for (int pixel = 0; pixel < 256; pixel++) {
-      WS2812B.setPixelColor(pixel, *(frame[pixel]));
+    for (uint16_t pixel = 0; pixel < NUM_PIXELS; pixel++) {
+      WS2812B.setPixelColor(pixel, frame[pixel]);
     }
 
     WS2812B.show();
-    Serial.println("Displayed frame");
-    delay(frame_time * 1000);
+    delay(1000);
   }
+  //   Serial.println("Displayed frame");
+  //   delay(frame_time * 1000);
+  // }
 }
--- esp32-test-pattern/test_pattern.h
+++ esp32-test-pattern/test_pattern.h
@@ -13,52 +13,57 @@ static uint32_t Color(uint8_t r, uint8_t g, uint8_t b) {
   return ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
 }
 
-uint32_t colors[5] = {
-    Color(255, 0, 0),    // Red
-    Color(255, 106, 0),  // Orange
-    Color(255, 216, 0),  // Yellow
-    Color(0, 255, 33),   // Green
-    Color(255, 255, 255) // White
-};
+// uint32_t colors[5] = {
+//     Color(255, 0, 0),    // Red
+//     Color(255, 106, 0),  // Orange
+//     Color(255, 216, 0),  // Yellow
+//     Color(0, 255, 33),   // Green
+//     Color(255, 255, 255) // White
+// };
 
-uint32_t* frames[2][256] = {
-    // Frame 1
-    {
-       /*0*/  &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0],
-       /*1*/  &colors[0], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[0],
-       /*2*/  &colors[0], &colors[4], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[4], &colors[0],
-       /*3*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*4*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*5*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*6*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[3], &colors[3], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*7*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[4], &colors[4], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*8*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[4], &colors[4], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*9*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[3], &colors[3], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*10*/ &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*11*/ &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*12*/ &colors[0], &colors[4], &colors[1], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[1], &colors[4], &colors[0],
-       /*13*/ &colors[0], &colors[4], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[4], &colors[0],
-       /*14*/ &colors[0], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[0],
-       /*15*/ &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0]
-    },
-    // Frame 2
-    {
-       /*0*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*1*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*2*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*3*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*4*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*5*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*6*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*7*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*8*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*9*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*10*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*12*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*13*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*14*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-       /*15*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
-    },
-};
+uint32_t frame_1[5] = {Color(255, 0, 0), Color(255, 0, 0), Color(255, 0, 0), Color(255, 0, 0), Color(255, 0, 0)};
+uint32_t frame_2[5] = {Color(255, 216, 0), Color(255, 216, 0), Color(255, 216, 0), Color(255, 216, 0), Color(255, 216, 0)};
 
-double frame_time = 1.0; // seconds per frame
+uint32_t* frames[2] = {frame_1, frame_2};
+
+// uint32_t* frames[2][256] = {
+//     // Frame 1
+//     {
+//        /*0*/  &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0],
+//        /*1*/  &colors[0], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[0],
+//        /*2*/  &colors[0], &colors[4], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[4], &colors[0],
+//        /*3*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*4*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*5*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*6*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[3], &colors[3], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*7*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[4], &colors[4], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*8*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[4], &colors[4], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*9*/  &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[3], &colors[3], &colors[3], &colors[3], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*10*/ &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*11*/ &colors[0], &colors[4], &colors[1], &colors[4], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[2], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*12*/ &colors[0], &colors[4], &colors[1], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[1], &colors[4], &colors[0],
+//        /*13*/ &colors[0], &colors[4], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[1], &colors[4], &colors[0],
+//        /*14*/ &colors[0], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[4], &colors[0],
+//        /*15*/ &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0], &colors[0]
+//     },
+//     // Frame 2
+//     {
+//        /*0*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*1*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*2*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*3*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*4*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*5*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*6*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*7*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*8*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*9*/  &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*10*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*12*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*13*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*14*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//        /*15*/ &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0], &colors[1], &colors[2], &colors[3], &colors[4], &colors[0],
+//     },
+// };
+
+// double frame_time = 1.0; // seconds per frame
